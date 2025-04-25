5ccf2d66af68e4fc530df61f52b0d92feac7e46c
jared <jargru@gmail.com>
2025-04-24 21:45:15 -0700

    fixed phrasing for HQs in log and added link to cs50

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
             <p>
                 This project is created and maintained by <a href='mailto:jargru@gmail.com'>Jared Matzecki</a>. Hi!
                 I'm a screenwriter by trade and a (relatively new) hobbyist programmer.
-                I made this as my final project for Harvard's online CS50 course.
+                I made this as my final project for Harvard's online <a href='https://cs50.harvard.edu/x/2025/' target='_blank'>CS50 course.</a>
             </p>
             <Footer />
         </div>
